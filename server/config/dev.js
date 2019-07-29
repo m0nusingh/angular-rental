0d340c0b98a378623670da0cf74e41fc122a581e
Monu Singh <33417737+1monusingh@users.noreply.github.com>
2019-07-29 12:22:01 +0530

    Delete Dev.js

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    URI: "mongodb+srv://rob:dfJ3eTUg9khzVBE3@myrental-kmjkc.mongodb.net/test?retryWrites=true&w=majority",
-    API:"AIzaSyBwT9K5QVZwcMcxAuizc4KspfiMHuZ8ek8"
-
-};
